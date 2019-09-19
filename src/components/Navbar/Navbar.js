cd30d4d96ebaa411bba04d4359a0f7837286f80f
IzaacAyelin <47495047+IzaacAyelin@users.noreply.github.com>
2019-09-19 14:59:57 +0300

    mobile navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import { Link } from "react-router-dom";
 import logo from '../../assets/images/logo.png'
 import './Navbar.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
+
+    const [navbar,setNavbar] = useState({
+        mobileClass:'navbar',
+        mobileMode: false
+    });
+    const toggleNavbar =()=>{
+        const className = navbar.mobileMode === false ? 'navbar show' : 'navbar';
+        setNavbar({
+            mobileClass: className,
+            mobileMode: !navbar.mobileMode   
+        })
+    }
+    
+
     return (
         <React.Fragment>
-            <div className="navbar">
+            <div className="hamburger">
+                <FontAwesomeIcon onClick={toggleNavbar} icon={faBars} />
+            </div>
+            <div className={navbar.mobileClass}>
                 <div className="logo">
                     <img src={logo} alt="logo" />
                 </div>
